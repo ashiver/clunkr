d3a6c7d02b0f2f9a1108697dae291572fadbffff
ashiver <anthony.lee.shiver@gmail.com>
2015-04-25 02:15:28 +0000

    Changed upload folder config

--- clunkr/__init__.py
+++ clunkr/__init__.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "clunkr.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 with open("database.cnf") as f:
     config = json.load(f)
     
